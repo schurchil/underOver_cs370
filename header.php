3f3852573f94035154fd5e806745ef37448b234a
schurchil <schurchill@mail.bradley.edu>
2025-04-11 11:50:20 -0500

    html and css jumping off piont

---
+++ header.php
@@ -0,0 +1,22 @@
+<?php?>
+
+
+<html>
+
+<div class="navbar">
+    <a href="#home">Home</a>
+    <a href="#news">Profile</a>
+    <div class="dropdown">
+        <button class="dropbtn">Actions
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+        </div>
+    </div>
+</div>
+
+
+</html>
---
+++ main.php
@@ -0,0 +1,14 @@
+
+<?php include_once("header.php"); ?>
+<html>
+
+
+<header>
+    <h1> Online Bank</h1>
+    <p>branch name</p>
+</header>
+
+
+</html>
+
+
